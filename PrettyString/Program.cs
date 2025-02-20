95f28e44fa74399a1645196aec57913967bd766f
Kadria Sultan <94522122+kadriasultan@users.noreply.github.com>
2025-02-20 22:41:49 +0100

    Add project files.

---
+++ PrettyString/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+public static class PrettyString
+{
+    public static string AddXs(string input)
+    {
+        return $"x{input}x";
+    }
+
+    public static string AddStars(string input)
+    {
+        return $"* {input}*";
+    }
+}
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string result1 = PrettyString.AddXs("Hallo");
+        Console.WriteLine(result1);
+
+
+        string result2 = PrettyString.AddStars("Hallo");
+        Console.WriteLine(result2);
+    }
+}
